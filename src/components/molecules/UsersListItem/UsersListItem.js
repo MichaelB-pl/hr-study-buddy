8b9238273be16f88ea84aeabd04b320a046496db
Michał Bachta <michal.bachta@gmail.com>
2021-03-29 10:15:11 +0200

    Styled Components

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './UsersListItem.module.scss';
+import styled from 'styled-components';
+import Button from 'components/atoms/Button/Button';
+
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(, :last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.lightGrey};
+  }
+`;
 
 const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
-  <li className={styles.container}>
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>attendance: {attendance}</p>
     </div>
-    <button>X</button>
-  </li>
+    <Button />
+  </Wrapper>
 );
 UsersListItem.propTypes = {
   userData: PropTypes.shape({
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import UsersList from 'components/UsersList/UsersList';
+import styled, { ThemeProvider } from 'styled-components';
+import UsersList from 'components/organisms/UsersList/UsersList';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { theme } from 'assets/styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 
 const Root = () => (
-  <>
-    <UsersList />
-  </>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Wrapper>
+      <UsersList />
+    </Wrapper>
+  </ThemeProvider>
 );
-Root.propTypes = {};
 
 export default Root;
