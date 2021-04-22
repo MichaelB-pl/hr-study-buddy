829ef82c9a6cde6e727adba6c5f8269cf87049ff
Michał Bachta <michal.bachta@gmail.com>
2021-04-22 10:18:19 +0200

    Tests for NewsSection and AddUser

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -5,7 +5,22 @@ import { renderWithProviders } from 'helpers/renderWithThemeProvider';
 import { fireEvent, screen } from '@testing-library/react';
 
 describe('Input With Button', () => {
-  it('Renders the component', () => {
+  it('Adds new user to the list', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>
+    );
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Grażyna' } });
+    fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+    fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
+    fireEvent.click(screen.getByTestId('Consent'));
+    fireEvent.click(screen.getByText('Add'));
+    screen.getByText('Grażyna');
+  });
+
+  it('Prevents adding new user if the consent is not checked', () => {
     renderWithProviders(
       <>
         <AddUser />
@@ -16,6 +31,7 @@ describe('Input With Button', () => {
     fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
     fireEvent.click(screen.getByText('Add'));
-    screen.getByText('Grażyna');
+    const newUser = screen.queryByText('Grażyna');
+    expect(newUser).not.toBeInTheDocument();
   });
 });
